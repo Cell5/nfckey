a768441a5d7c210ac379c0f9e55045823515a1c0
Cell5 <exride@gmail.com>
2018-11-07 22:32:37 +1300

    DB restructurisation, added admin classes

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,48 @@
 from django.contrib import admin
-from core.models import Location, Receiver, SecurityGroup, Employee
+from .models import Location, Receiver, SecurityGroup, Employee
 
-admin.site.register(Location)
-admin.site.register(Receiver)
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('location', 'comment')
+    search_fields = ('location', 'comment')
+
+
+class ReceiverAdmin(admin.ModelAdmin):
+    list_display = ('receiver_dev_id', 'receiver_location')
+    search_fields = ('receiver_dev_id', 'receiver_location')
+
+
+# class SecurityGroupAdmin(admin.ModelAdmin):
+#     list_display = ('security_group_name',)
+#     search_fields = ('security_group_name', 'access_level')
+#     filter_horizontal = ('access_level',)
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        'login',
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'is_active',
+        # 'security_group',
+        'date_joined',
+        'nfc_dev_id',
+    )
+    search_fields = (
+        'login',
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'nfc_dev_id',
+    )
+
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Receiver, ReceiverAdmin)
 admin.site.register(SecurityGroup)
-admin.site.register(Employee)
+admin.site.register(Employee, EmployeeAdmin)
 
+admin.site.site_header = 'NFC Key Administration'
--- core/models.py
+++ core/models.py
@@ -13,7 +13,7 @@ class Location(models.Model):
     )
     location_id = models.AutoField(primary_key=True)
     location = models.CharField(max_length=16, choices=LOCATION_CHOICES, null=False, unique=True)
-    comment = models.CharField(max_length=128, null=True)
+    comment = models.CharField(max_length=128, null=True, blank=True)
 
     def __str__(self):
         return self.location
@@ -25,13 +25,13 @@ class Receiver(models.Model):
     receiver_location = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return '%s %s' % (self.receiver_dev_id, self.receiver_location)
+        return self.receiver_location
 
 
 class SecurityGroup(models.Model):
     security_group_id = models.AutoField(primary_key=True)
     security_group_name = models.CharField(max_length=64, null=False, unique=True, default='')
-    access_level = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True)
+    access_level = models.ManyToManyField(Location)
 
     def __str__(self):
         return self.security_group_name
@@ -47,11 +47,11 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=512, null=False)
     last_name = models.CharField(max_length=512, null=False)
     email = models.EmailField(max_length=254, null=False, unique=True)
+    phone = models.CharField(max_length=15, null=True, blank=True, default='')
     is_active = models.CharField(max_length=8, choices=YES_NO_CHOICES, default='No')
     date_joined = models.DateTimeField(auto_now_add=True, null=True)
-    security_group = models.ForeignKey('SecurityGroup', on_delete=models.SET_NULL, null=True)
+    security_group = models.ManyToManyField(SecurityGroup, blank=True)
     nfc_dev_id = models.CharField(max_length=254, null=False, default='')
-    phone = models.CharField(max_length=15, null=True, default='')
 
     def __str__(self):
         return '%s %s' % (self.first_name, self.last_name)
